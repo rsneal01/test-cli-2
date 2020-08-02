441694dd97e78cbb148234893eada883a0de23a8
Bobby Neal <rsneal01@gmail.com>
2020-08-02 16:08:37 +0000

    first commit

---
+++ lib/test/cli/2.rb
@@ -0,0 +1,10 @@
+require "test/cli/2/version"
+
+module Test
+  module Cli
+    module 2
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/test/cli/2/version.rb
@@ -0,0 +1,7 @@
+module Test
+  module Cli
+    module 2
+      VERSION = "0.1.0"
+    end
+  end
+end
